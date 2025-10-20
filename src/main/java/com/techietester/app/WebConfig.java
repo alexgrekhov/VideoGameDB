0f3eac2fb78d40ea83e7bd0e9943146998b0e668
Alex Grekhov <galexalex801@gmail.com>
2025-10-20 17:40:56 +0300

    Сделал ссconfig props

--- src/main/java/com/techietester/app/WebConfig.java
+++ src/main/java/com/techietester/app/WebConfig.java
@@ -1,33 +1,26 @@
 package com.techietester.app;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/",
-            "classpath:/resources/",
-            "classpath:/static/",
-            "classpath:/public/"
-    };
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // Swagger UI
         registry.addResourceHandler("/swagger-ui/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.52.5/");
-        // Остальные статические ресурсы
-        registry.addResourceHandler("/**")
-                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/", "/index.html");
+        // Редирект с корня и swagger-ui.html на index.html
+        registry.addRedirectViewController("/", "/swagger-ui/index.html");
         registry.addRedirectViewController("/swagger-ui.html", "/swagger-ui/index.html");
     }
 }
